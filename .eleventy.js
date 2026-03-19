8f81d4c58ee0ae2fcac547c9c3d298590ff7b7f3
alexdking98 <alexdking98@gmail.com>
2026-03-19 15:08:48 -0400

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,35 @@
+module.exports = function(eleventyConfig) {
+  // Copy static assets
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("robots.txt");
+  eleventyConfig.addPassthroughCopy("sitemap.xml");
+
+  // Add date filter for formatting dates
+  eleventyConfig.addFilter("dateDisplay", function(date) {
+    return new Date(date).toLocaleDateString('en-US', { 
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric' 
+    });
+  });
+
+  // Add filter for excerpt
+  eleventyConfig.addFilter("excerpt", function(content) {
+    return content.substring(0, 200) + "...";
+  });
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
+  };
+};
